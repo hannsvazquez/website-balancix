f8342bbc9a0e90813e614ace7f6f873756504012
Hann Vasquez <balancix3@gmail.com>
2025-04-26 20:31:10 -0500

    Adding the sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 import { remarkReadingTime } from './remark-reading-time.mjs';
 
 import tailwind from '@astrojs/tailwind';
@@ -8,7 +9,20 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  site: 'https://balancix.site',
+  integrations: [
+    tailwind(),
+    icon(),
+    sitemap({
+      i18n: {
+        defaultLocale: 'es',
+        locales: {
+          en: 'en',
+          es: 'es'
+        }
+      }
+    })
+  ],
   markdown: {
     syntaxHighlight: "shiki",
     shikiConfig: {

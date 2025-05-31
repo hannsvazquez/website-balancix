a2220b1d1cd65808dad4da3c836d224353e5be3c
HannVasquez <balancix3@gmail.com>
2025-05-31 15:29:13 -0500

    Adding more translation to the English version

--- src/i18n/translations.ts
+++ src/i18n/translations.ts
@@ -26,7 +26,13 @@ export const translations = {
         'career.subtitle': 'Prepárate para trabajar como:',
 
         'review.1': 'La misión de Balancix es cerrar la brecha entre la formación universitaria y las demandas del mercado laboral actual. Buscamos que más jóvenes accedan a oportunidades en la industria tech con salarios competitivos y un equilibrio entre el trabajo y vida personal, todo bajo la modalidad de trabajar desde cualquier parte del mundo.',
-        'review.2': 'Balancix es una iniciativa peruana innovadora, con un enfoque totalmente práctico para preparar a los estudiantes en las posiciones más demandadas del sector tech. Lo que realmente destaco es su red de contactos en el mundo de las startups, lo que brinda una gran ventaja a quienes buscan empezar su carrera profesional en este sector que brinda mucha flexibilidad pero a la vez con grandes retos.'
+        'review.2': 'Balancix es una iniciativa peruana innovadora, con un enfoque totalmente práctico para preparar a los estudiantes en las posiciones más demandadas del sector tech. Lo que realmente destaco es su red de contactos en el mundo de las startups, lo que brinda una gran ventaja a quienes buscan empezar su carrera profesional en este sector que brinda mucha flexibilidad pero a la vez con grandes retos.',
+
+        'steps.title': 'Empezar',
+
+        'pricing.title': 'Precios',
+        'cta.main': '¿Aún no sabes en qué posición especializarte?',
+
     },
     en: {
         'nav.home': 'Home',
@@ -48,6 +54,11 @@ export const translations = {
         'career.subtitle': 'Get ready to work as:',
 
         'review.1': "Balancix's mission is to bridge the gap between university education and current job market demands. We seek to enable more young people to access opportunities in the tech industry with competitive salaries and work-life balance, all under the modality of working from anywhere in the world.",
-        'review.2': "Balancix is an innovative Peruvian initiative with a totally practical approach to preparing students for the most in-demand positions in the tech sector. What I really highlight is their network of contacts in the startup world, which provides a great advantage to those looking to start their professional career in this sector that offers a lot of flexibility but at the same time with great challenges."
+        'review.2': "Balancix is an innovative Peruvian initiative with a totally practical approach to preparing students for the most in-demand positions in the tech sector. What I really highlight is their network of contacts in the startup world, which provides a great advantage to those looking to start their professional career in this sector that offers a lot of flexibility but at the same time with great challenges.",
+
+        'steps.title': 'Get Started Now',
+        'pricing.title': 'Pricing',
+
+        'cta.main': 'Still not sure where you can start?',
     }
 };
